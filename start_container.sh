703bebdc0c5350718e5d02784370d464f1140601
hehongyuan <1014596312@qq.com>
2018-05-16 17:20:08 +0800

    [Opt] Add start_container.sh to pull and restart the latest epg container

---
+++ start_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+EPG_DOCKER_IMAGE=registry.cn-hangzhou.aliyuncs.com/tongshi/epg:latest
+# pull the latest epg image from registry
+docker pull $EPG_DOCKER_IMAGE
+# stop and remove existing epg container
+docker rm -f epg > /dev/null
+# run the latest epg container
+docker run -p 10010:10010 \
+-v /home/share/epg_server/conf:/app/conf \
+-d \
+--restart=always \
+--name epg \
+$EPG_DOCKER_IMAGE
