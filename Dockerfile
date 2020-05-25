38b9cb326d66227a2abcb733f3ba950defc7d84d
bearguo <783078153@qq.com>
2020-05-25 14:43:46 +0800

    通过第三方获取数据并缓存，不再对外开放

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,6 @@ COPY conf/epg.conf /app/conf/
 
 HEALTHCHECK --interval=5s --timeout=3s CMD curl -fs http://localhost:10010/EPG/channel?secret=VYDcCe1s || exit 1
 
-EXPOSE 10010
-
 CMD ["python", "epg.py"]
 RUN apk add --no-cache tzdata curl \
     && ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \ 
--- epg.py
+++ epg.py
@@ -1,15 +1,12 @@
 from flask import (
     Flask,
     request,
-    make_response,
-    url_for,
-    redirect
+    make_response
 )
 from flask_caching import Cache
 from flask_cors import CORS
 import os, sys, logging
 from logging.handlers import RotatingFileHandler
-import configparser
 from pathlib import Path
 from urllib.parse import urlencode
 from urllib.request import urlopen
@@ -18,25 +15,17 @@ if getattr(sys, 'frozen', False):
     cur_path = os.path.dirname(sys.executable)
 elif __file__:
     cur_path = os.path.dirname(os.path.realpath(__file__))
-log_file_name = str(Path(cur_path) / 'conf' / 'epg.log')
+log_file_name = str(Path(cur_path) / 'log' / 'epg.log')
 log_file_handler = RotatingFileHandler(filename=log_file_name, maxBytes=10*1024*1024, backupCount=3)
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s',
                     handlers=[log_file_handler]
                     )
 logging.info('log file created')
-PORT = 10010
 
-try:
-    cf = configparser.ConfigParser()
-    cf.read(str(Path(cur_path) / 'conf' / 'epg.conf'))
-    THIRD_PARTY_EPG_URL_BASE = cf.get('epg_master', 'base_url')
-    PORT = cf.get('server', 'port')
-    logging.info('epg slave port is %s' % PORT)
-    logging.info('epg master url is: %s' % THIRD_PARTY_EPG_URL_BASE)
-except Exception as e:
-    logging.exception('Parse configuration file failed!')
-    sys.exit(-1)
+MASTER_PATH = "http://api.deepepg.com/api"
+PORT = 10009
+SECRET_KEY = 'VYDcCe1s'
 
 app = Flask('__name__')
 CORS(app)
@@ -44,31 +33,15 @@ cache = Cache()
 cache.init_app(app,config={
     "CACHE_TYPE":"simple",
     "CACHE_DEFAULT_TIMEOUT": 600,})
-SECRET_KEY = 'VYDcCe1s'
 
 @app.route('/EPG/channel', methods=['GET'])
 @cache.cached(timeout=3600)
 def channel():
-    """
-    Cache the 3rd party epg server channel api.
-
-    :return: xml page from 3rd party epg server.
-    """
-    # 1. Check the secret key value.
-    secret_key = request.args.get('secret', None)
-    if secret_key is None:
-        error_message = 'missing the secret key! please check the url!'
-        logging.error(error_message)
-        return error_message
-    elif secret_key != SECRET_KEY:
-        error_message = 'wrong secret key!'
-        logging.error(error_message)
-        return error_message
 
     params = {
         'secret': SECRET_KEY,
     }
-    url = '%s/%s?%s' % (THIRD_PARTY_EPG_URL_BASE, 'channel', urlencode(params))
+    url = '%s/%s?%s' % (MASTER_PATH, 'channel', urlencode(params))
     try:
         web_page = urlopen(url, timeout=20).read()
     except Exception as e:
@@ -82,22 +55,11 @@ def channel():
 @app.route("/EPG/schedule/<channelName>", methods=["GET"])
 @cache.cached()
 def program(channelName):
-    '''
-    secret_key = request.args.get('secret', None)
-    if secret_key is None:
-        error_message = 'missing the secret key! please check the url!'
-        logging.error(error_message)
-        return error_message
-    elif secret_key != SECRET_KEY:
-        error_message = 'wrong secret key!'
-        logging.error(error_message)
-        return error_message
-    '''
     params = {
         'secret': SECRET_KEY,
         'id': channelName
     }
-    url = '%s/%s?%s' % (THIRD_PARTY_EPG_URL_BASE, 'schedule', urlencode(params))
+    url = '%s/%s?%s' % (MASTER_PATH, 'schedule', urlencode(params))
     try:
         web_page = urlopen(url, timeout=20).read()
     except Exception as e:
